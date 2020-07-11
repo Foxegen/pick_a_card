b0e40224bf69d87b754e45ff9ae31e6e84ca9e0a
Foxegen <foxegen14@gmail.com>
2020-07-11 15:13:10 +0300

    Update suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
 values = ['2','3','4','5','6','7','8','9','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
